e4430643a71fae3d23242ddf72d7f0288f2e3689
Dardeka <ddkwari2@gmail.com>
2025-04-07 21:11:02 +0700

    Adding if statement into the function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
-// Adding new node
-fn insert_data(data: i32, mut &root){
-    if root == None{
-        &root = data;
-    }   
-}
+// // Adding new node
+// fn insert_data(data: i32, mut &root){
+//     if root == None{
+//         &root = data;
+//     }   
+// }
 
 // Creating new tree 
-fn new_tree(){
+// fn new_tree(){
 
-}
+// }
 
 // BST function
-fn bst(arr: [i32; 8]) {
-    let mut root: Option<i32> = None;
-    for x in arr{
-        insert_data(x, &root);
+fn bst(arr: &mut[i32], idx: usize) {
+    // print!("{:?}\n", arr);
+
+    // let root = arr[idx];
+
+    // State parent, left, and right index
+    let parent = idx;
+    let left = idx*2+1;
+    let right = idx*2+2;
+
+    println!("Array has been accepted! parent: {} ; left: {}; right: {}", arr[idx], arr[left], arr[right]);
+
+    if arr[left]>arr[parent]{
+        let temp = arr[right];
+        arr[right] = arr[left];
+        arr[left] = temp;
     }
+    if arr[right]<arr[parent] {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+    println!(" ");
+    println!("Array has been accepted! parent: {} ; left: {}; right: {}", arr[idx], arr[left], arr[right]);
 }
 
 fn main() {
-    let array = [8,4,9,5,6,2,11,10];
-    bst(array);
+    let mut array= [8,9,4,5,6,2,11,10];
+    bst(&mut array, 0);
 }
