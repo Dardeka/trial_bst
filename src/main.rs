900ebc113eda8bbf54d06dd028ff217f25e67e7d
Dardeka <ddkwari2@gmail.com>
2025-04-10 19:42:58 +0700

    Adding full code of in-order, minimum, and maximum functions into the main file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// #[derive(Debug)]
+#[derive(Debug)]
 struct Node{
     key : i32,
     left: Option<Box<Node>>,
@@ -6,18 +6,8 @@ struct Node{
 }
 
 impl Node {
-    // fn new_node(val: i32)-> Option<Box<Node>>{
-    //     Some(
-    //         Box::new(
-    //             Node { 
-    //                 key: val, 
-    //                 left: None, 
-    //                 right: None, 
-    //             }
-    //         )
-    //     )
-    // }
-
+    
+    // To insert new node into the tree by checking the current tree either have a root or not
     fn insert_node(root: Option<Box<Node>>, val: i32) -> Option<Box<Node>>{
         match root {
             Some(mut node) => {
@@ -40,8 +30,54 @@ impl Node {
         }
     }
 
-    fn inorder(){
-        
+    // to make inorder tree walk
+    fn inorder(root: Option<Box<Node>>){
+        if let Some(node ) = root {
+            Self::inorder(node.left);
+            print!("{} ", node.key);
+            Self::inorder(node.right);
+        }
+    }
+    
+    // To find the minimum value of the tree
+    fn minimum(root: Option<Box<Node>>){
+        if let Some(node) = root{
+            if node.left.is_some(){
+                Self::minimum(node.left);
+            }else{
+                println!("The minimum Node is: {}", node.key);
+            }
+        }
+    }
+
+    // To find the maximum value of the tree
+    fn maximum(root: Option<Box<Node>>){
+        if let Some(node) = root{
+            if node.right.is_some(){
+                Self::maximum(node.right);
+            }else{
+                println!("The maximum Node is: {}", node.key);
+            }
+        }
+    }
+
+    fn successor(root: Option<Box<Node>>, key: i32){
+        if let Some(node) = root{
+            if node.key == key {
+                if node.right.is_some(){
+                    let root = node.right;
+                    if node.left.is_some(){
+                        // let succ = Node::minimum(root);
+                        // println!("The successor of {} is {:?}", key, succ);
+                        println!("The key is: {}", node.key);
+                    }else {
+                        
+                    }
+                }
+            }else{
+
+            }
+        }
     }
 }
 
@@ -52,11 +88,24 @@ fn bst(arr: &mut [i32]){
         root = Node::insert_node(root, arr[index]);
         index += 1;
     }
+    
+    // // Inorder Tree Walk
+    // print!("The in-order tree walk is: ", );
+    // Node::inorder(root);
+    // print!("\n");
 
-    println!("{:?}", root)
+    // // Minimum Tree
+    // Node::minimum(root);
+
+    // // Maximum Tree
+    // Node::maximum(root);
+
+    // Successor
+    Node::successor(root, 15);
+
 }
 
 fn main() {
-    let mut array= [8,9,4,5,6,2,11,10];
+    let mut array= [15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9];
     bst(&mut array);
 }
